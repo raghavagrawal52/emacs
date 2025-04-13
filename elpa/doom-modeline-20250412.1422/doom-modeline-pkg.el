3413102446081c41562910c48248753743c47f05
raghavagrawal52 <raghavagrawal.g@gmail.com>
2025-04-13 15:25:00 +0530

    initial commit

---
+++ elpa/doom-modeline-20250412.1422/doom-modeline-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "doom-modeline" "20250412.1422"
+  "A minimal and modern mode-line."
+  '((emacs       "25.1")
+    (compat      "29.1.4.5")
+    (nerd-icons  "0.1.0")
+    (shrink-path "0.3.1"))
+  :url "https://github.com/seagle0128/doom-modeline"
+  :commit "d56c1ff69174955e8ab75195094ad6cb928961c8"
+  :revdesc "d56c1ff69174"
+  :keywords '("faces" "mode-line")
+  :authors '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers '(("Vincent Zhang" . "seagle0128@gmail.com")))
---
+++ elpa/undo-fu-20241206.219/undo-fu-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "undo-fu" "20241206.219"
+  "Undo helper with redo."
+  '((emacs "25.1"))
+  :url "https://codeberg.org/ideasman42/emacs-undo-fu"
+  :commit "399cc12f907f81a709f9014b6fad0205700d5772"
+  :revdesc "399cc12f907f"
+  :authors '(("Campbell Barton" . "ideasman42@gmail.com"))
+  :maintainers '(("Campbell Barton" . "ideasman42@gmail.com")))
